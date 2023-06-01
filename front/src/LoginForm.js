e0aa59a2fc9697ac2e2f602d168c453ece657058
Gael16062003 <ambeugael128@gmail.com>
2023-06-01 14:50:01 +0200

    création du formulaire

---
+++ front/src/LoginForm.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: email,
+      password: password
+    };
+
+    try {
+      const response = await axios.post('votre-url-de-connexion', data);
+      console.log(response.data); // Faites quelque chose avec la réponse
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <br />
+      <button type="submit">Se connecter</button>
+    </form>
+  );
+};
+
+export default LoginForm;
