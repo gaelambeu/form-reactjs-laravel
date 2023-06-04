93072a337dcc9b7ce7dbe154ec475e7908e2b39d
Gael16062003 <ambeugael128@gmail.com>
2023-06-05 01:00:06 +0200

    modif1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,17 @@ function App() {
 
   return (
     <div className="max-w-6xl mx-auto">
-      <h1>Welcome</h1>
+      <nav className="rounded bg-indigo-900 text-white px-2 py-2.5 sm:px-4">
+        <div 
+          className="container mx-auto flex flex-wrap items-center justify-between"
+          bis_skin_checked="1"
+        >
+          <a href="https://laraveller.com/" className="flex items-center">
+            laraveller
+          </a>
+
+        </div>
+      </nav>
     </div>
   )
 }
